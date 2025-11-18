43035d40a42f6437453b3acdb8bc4d58d93d73d1
SysoevAS <invetys@gmail.com>
2025-11-18 19:46:47 +0300

    First part

---
+++ src/primitive_db/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Точка входа в проект primitive_db."""
+
+
+def main() -> None:
+    """Выводит простое сообщение при запуске пакета."""
+    print("DB project is running!")
+
+
+if __name__ == "__main__":
+    main()
